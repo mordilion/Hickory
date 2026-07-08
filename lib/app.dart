f382ef19e1d68e4238362239b5aa7b5b249206a5
mordilion <mordilion@gmx.de>
2026-07-08 07:57:16 +0200

    Wire WindowTrayController into app startup

--- lib/app.dart
+++ lib/app.dart
@@ -4,7 +4,9 @@ import 'core/theme/app_theme.dart';
 import 'features/shell/app_shell.dart';
 
 class HickoryApp extends StatelessWidget {
-  const HickoryApp({super.key});
+  const HickoryApp({super.key, required this.scaffoldMessengerKey});
+
+  final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey;
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +14,7 @@ class HickoryApp extends StatelessWidget {
       title: 'Hickory',
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
+      scaffoldMessengerKey: scaffoldMessengerKey,
       home: const AppShell(),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app.dart';
+import 'core/window/window_tray_controller.dart';
 
-void main() {
-  runApp(const ProviderScope(child: HickoryApp()));
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final windowTrayController = WindowTrayController();
+  await windowTrayController.initialize();
+
+  runApp(
+    ProviderScope(
+      child: HickoryApp(scaffoldMessengerKey: windowTrayController.scaffoldMessengerKey),
+    ),
+  );
 }
