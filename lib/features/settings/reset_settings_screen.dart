c7234a75a07230e38bd973208fd272b80adc2548
mordilion <mordilion@gmx.de>
2026-08-08 23:49:29 +0200

    feat(settings): add ResetSettingsScreen

---
+++ lib/features/settings/reset_settings_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'app_reset_section.dart';
+import 'settings_sub_page.dart';
+
+/// No page title -- AppResetSection already renders l10n.settingsResetTitle
+/// as its own heading (see settings_sub_page.dart's doc comment).
+class ResetSettingsScreen extends StatelessWidget {
+  const ResetSettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SettingsSubPage(
+      child: Card(
+        child: Padding(padding: EdgeInsets.all(16), child: AppResetSection()),
+      ),
+    );
+  }
+}
