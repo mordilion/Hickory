dcbc8dbd079fb426d99fd26e262686194c3858f0
mordilion <mordilion@gmx.de>
2026-08-08 23:45:01 +0200

    feat(settings): add ProjectsSettingsScreen

---
+++ lib/features/settings/projects_settings_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import '../projects/projects_editor.dart';
+import 'settings_sub_page.dart';
+
+/// No page title -- ProjectsEditor already renders l10n.settingsProjectsTitle
+/// as its own heading (see settings_sub_page.dart's doc comment).
+class ProjectsSettingsScreen extends StatelessWidget {
+  const ProjectsSettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SettingsSubPage(
+      child: Card(
+        child: Padding(padding: EdgeInsets.all(16), child: ProjectsEditor()),
+      ),
+    );
+  }
+}
